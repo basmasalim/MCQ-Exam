6237659723bce9182d780b288009a2c8a0ce0cbd
basmasalim <basmasalim030@gmail.com>
2026-02-09 15:59:20 +0200

    RegisterComponent

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ButtonComponent } from '../../../shared/ui/button/button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ButtonComponent, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  // UI
+  btnText = signal('Create Account');
+  btnClasses = signal('w-full');
 
+  handleClick() {
+    this.btnText.set('Clicked ✅');
+  }
 }
